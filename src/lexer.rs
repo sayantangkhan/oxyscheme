2525fe20d1ba8465ab5fba77f9f844065b13ed9f
Sayantan Khan <sayantangkhan@gmail.com>
2020-12-27 17:17:44 +0530

    Finished the elementary lexers

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,29 +1,50 @@
-use nom::branch::alt;
-use nom::bytes::complete::{escaped_transform, is_not, tag};
-use nom::character::complete::{anychar, one_of};
-use nom::error::ErrorKind;
-use nom::IResult;
+//! Module to lex the input stream and return a stream of tokens
 use nom::{
-    character::complete::satisfy,
-    combinator::{map, peek, value},
+    branch::alt,
+    bytes::complete::{escaped_transform, is_not, tag},
+    character::complete::{anychar, digit0, digit1, one_of, satisfy},
+    combinator::{map, opt, peek, recognize, value},
+    error::ErrorKind,
     multi::many0,
+    sequence::tuple,
+    IResult,
 };
 
-enum Token {
+use nom::error::Error as NomErrorStruct;
+use nom::Err::Error as NomErrorEnum;
+
+/// Type alias for the common return type for the lexers
+type LexResult<'a> = IResult<&'a str, Token<'a>, NomErrorStruct<&'a str>>;
+
+/// Terminal token types for the lexer
+///
+/// The variants of `Token` wrap around the corresponding Rust types in the case of `String`,
+/// `Character`, and `Boolean`. `Number` wraps around `LispNum`, which can either be an `f32`
+/// or an `i32`. `Identifier` and `Punctuator` wrap around slices from the input, to avoid
+/// unnecessary heap copying and heap allocation. In particular, this means that the token cannot
+/// be dropped before the input string.
+#[derive(Debug, PartialEq)]
+pub enum Token<'a> {
     String(String),
     Character(char),
     Boolean(bool),
     Number(LispNum),
-    Identifier(String),
-    Punctuator(String),
+    Identifier(&'a str),
+    Punctuator(&'a str),
 }
 
-enum LispNum {
+/// Internal representation of numeric types in Scheme
+///
+/// `LispNum` is an enum wrapping around Rust's `i32` and `f32` types; the only two numeric types
+/// we are currently implementing for the Scheme compiler target. More variants will be added in
+/// the future.
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum LispNum {
     Integer(i32),
     Float(f32),
 }
 
-fn parse_string(input: &str) -> IResult<&str, String> {
+fn parse_string(input: &str) -> LexResult {
     let (input, _) = tag("\"")(input)?;
     let (leftover, parsed) = escaped_transform(
         is_not("\\\""),
@@ -35,19 +56,16 @@ fn parse_string(input: &str) -> IResult<&str, String> {
         )),
     )(input)?;
     let (input, _) = tag("\"")(leftover)?;
-    Ok((input, parsed))
+    Ok((input, Token::String(parsed)))
 }
 
-fn parse_boolean(input: &str) -> IResult<&str, bool> {
+fn parse_boolean(input: &str) -> LexResult {
     let (input, _) = tag("#")(input)?;
     let (leftover, parsed) = one_of("tf")(input)?;
     match parsed {
-        't' => Ok((leftover, true)),
-        'f' => Ok((leftover, false)),
-        _ => Err(nom::Err::Error(nom::error::Error::new(
-            input,
-            ErrorKind::OneOf,
-        ))),
+        't' => Ok((leftover, Token::Boolean(true))),
+        'f' => Ok((leftover, Token::Boolean(false))),
+        _ => Err(NomErrorEnum(NomErrorStruct::new(input, ErrorKind::OneOf))),
     }
 }
 
@@ -57,114 +75,230 @@ fn peek_delimiter(input: &str) -> IResult<&str, ()> {
     map(peek(delimiter), |_: char| ())(input)
 }
 
-fn parse_character(input: &str) -> IResult<&str, char> {
+fn parse_character(input: &str) -> LexResult {
     let (input, _) = tag("#\\")(input)?;
     let space_parser = map(tag("space"), |_| ' ');
     let newline_parser = map(tag("newline"), |_| '\n');
     let (leftover, parsed) = alt((space_parser, newline_parser, anychar))(input)?;
     peek_delimiter(leftover)?;
-    Ok((leftover, parsed))
+    Ok((leftover, Token::Character(parsed)))
 }
 
-fn non_peculiar(input: &str) -> IResult<&str, String> {
+fn non_peculiar(input: &str) -> IResult<&str, &str> {
     let special_initial = one_of("!$%&*/:<=>?^_~");
     let letter = satisfy(|c| c.is_alphabetic());
-    let mut initial = alt((letter, special_initial));
-    let (leftover, initial_parsed_char) = initial(input)?;
-
+    let initial = alt((letter, special_initial));
     let digit = satisfy(|c| c.is_numeric());
     let special_subsequent = one_of("+-.@");
-    let (leftover, mut vec_subsequent) =
-        many0(alt((initial, digit, special_subsequent)))(leftover)?;
+    let subsequent = alt((initial, digit, special_subsequent));
 
-    vec_subsequent.insert(0, initial_parsed_char);
+    // The repeated code is to get around the compiler's move semantics.
+    let special_initial = one_of("!$%&*/:<=>?^_~");
+    let letter = satisfy(|c| c.is_alphabetic());
+    let initial = alt((letter, special_initial));
 
-    Ok((leftover, vec_subsequent.iter().collect()))
+    recognize(tuple((initial, many0(subsequent))))(input)
 }
 
-fn parse_identifier(input: &str) -> IResult<&str, String> {
-    let peculiar_identifier = map(alt((tag("+"), tag("-"), tag("..."))), |s: &str| {
-        String::from(s)
-    });
+fn parse_identifier(input: &str) -> LexResult {
+    let peculiar_identifier = alt((tag("+"), tag("-"), tag("...")));
     let (leftover, parsed) = alt((non_peculiar, peculiar_identifier))(input)?;
     peek_delimiter(leftover)?;
-    Ok((leftover, parsed))
+    Ok((leftover, Token::Identifier(parsed)))
 }
 
-#[test]
-fn parse_string_test() {
-    assert_eq!(parse_string("\"string\""), Ok(("", String::from("string"))));
-    assert_eq!(
-        parse_string("\"st\\\"ring\""),
-        Ok(("", String::from("st\"ring")))
-    );
-    assert_eq!(
-        parse_string("\"fail"),
-        Err(nom::Err::Error(nom::error::Error::new("", ErrorKind::Tag)))
-    );
-    assert_eq!(
-        parse_string("\"new\\nline\""),
-        Ok(("", String::from("new\nline")))
-    );
-    assert_eq!(
-        parse_string("blah\"string\""),
-        Err(nom::Err::Error(nom::error::Error::new(
-            "blah\"string\"",
-            ErrorKind::Tag
-        )))
-    );
+fn parse_number(input: &str) -> LexResult {
+    let integer_parser = tuple((opt(one_of("+-")), digit1));
+    let float_parser =
+        tuple::<_, _, (_, ErrorKind), _>((opt(one_of("+-")), digit0, tag("."), digit1));
+    // Note that one needs to annotate the tuple function in this case because the compilier
+    // is unable to infer the return type.
+    match recognize(float_parser)(input) {
+        Ok((l, p)) => {
+            let parsed_num = match p.parse() {
+                Ok(num) => LispNum::Float(num),
+                Err(_) => {
+                    return Err(NomErrorEnum(NomErrorStruct::new(l, ErrorKind::TooLarge)));
+                }
+            };
+            Ok((l, Token::Number(parsed_num)))
+        }
+        Err(_) => {
+            let (l, p) = recognize(integer_parser)(input)?;
+            let parsed_num = match p.parse() {
+                Ok(num) => LispNum::Integer(num),
+                Err(_) => {
+                    return Err(NomErrorEnum(NomErrorStruct::new(l, ErrorKind::TooLarge)));
+                }
+            };
+            Ok((l, Token::Number(parsed_num)))
+        }
+    }
 }
 
-#[test]
-fn parse_boolean_test() {
-    assert_eq!(parse_boolean("#t"), Ok(("", true)));
-    assert_eq!(parse_boolean("#f"), Ok(("", false)));
-    assert_eq!(
-        parse_boolean("#m"),
-        Err(nom::Err::Error(nom::error::Error::new(
-            "m",
-            ErrorKind::OneOf
-        )))
-    );
+fn parse_punctuator(input: &str) -> LexResult {
+    alt((
+        tag("("),
+        tag(")"),
+        tag("#("),
+        tag("'"),
+        tag("`"),
+        tag(",@"),
+        tag(","),
+        tag("."),
+    ))(input)
+    .map(|(l, p)| (l, Token::Punctuator(p)))
 }
 
-#[test]
-fn parse_character_test() {
-    assert_eq!(parse_character("#\\n\n"), Ok(("\n", 'n')));
-    assert_eq!(parse_character("#\\space\n"), Ok(("\n", ' ')));
-    assert_eq!(parse_character("#\\newline\n"), Ok(("\n", '\n')));
-}
+#[cfg(test)]
+mod test {
 
-#[test]
-fn non_peculiar_identifier_test() {
-    assert_eq!(non_peculiar("a"), Ok(("", String::from("a"))));
-    assert_eq!(non_peculiar("a+"), Ok(("", String::from("a+"))));
-    assert_eq!(non_peculiar("&a+"), Ok(("", String::from("&a+"))));
-    assert_eq!(
-        non_peculiar("+&a+"),
-        Err(nom::Err::Error(nom::error::Error::new(
-            "+&a+",
-            ErrorKind::OneOf
-        )))
-    );
-}
+    use super::*;
 
-#[test]
-fn parse_identifier_test() {
-    assert_eq!(parse_identifier("...\n"), Ok(("\n", String::from("..."))));
-    assert_eq!(parse_identifier("var\n"), Ok(("\n", String::from("var"))));
-    assert_eq!(
-        parse_identifier("..."),
-        Err(nom::Err::Error(nom::error::Error::new(
-            "",
-            ErrorKind::OneOf
-        )))
-    );
-    assert_eq!(
-        parse_identifier("asdf,"),
-        Err(nom::Err::Error(nom::error::Error::new(
-            ",",
-            ErrorKind::OneOf
-        )))
-    );
+    #[test]
+    fn parse_string_test() {
+        assert_eq!(
+            parse_string(r#""string""#),
+            Ok(("", Token::String(String::from("string"))))
+        );
+        assert_eq!(
+            parse_string(r#""st\"ring""#),
+            Ok(("", Token::String(String::from("st\"ring"))))
+        );
+        assert_eq!(
+            parse_string(r#""fail"#),
+            Err(NomErrorEnum(NomErrorStruct::new("", ErrorKind::Tag)))
+        );
+        assert_eq!(
+            parse_string(r#""new\nline""#),
+            Ok(("", Token::String(String::from("new\nline"))))
+        );
+        assert_eq!(
+            parse_string(r#"blah"string""#),
+            Err(NomErrorEnum(NomErrorStruct::new(
+                "blah\"string\"",
+                ErrorKind::Tag
+            )))
+        );
+    }
+
+    #[test]
+    fn parse_boolean_test() {
+        assert_eq!(parse_boolean("#t"), Ok(("", Token::Boolean(true))));
+        assert_eq!(parse_boolean("#f"), Ok(("", Token::Boolean(false))));
+        assert_eq!(
+            parse_boolean("#m"),
+            Err(NomErrorEnum(NomErrorStruct::new("m", ErrorKind::OneOf)))
+        );
+    }
+
+    #[test]
+    fn parse_character_test() {
+        assert_eq!(parse_character("#\\n\n"), Ok(("\n", Token::Character('n'))));
+        assert_eq!(
+            parse_character("#\\space\n"),
+            Ok(("\n", Token::Character(' ')))
+        );
+        assert_eq!(
+            parse_character("#\\newline\n"),
+            Ok(("\n", Token::Character('\n')))
+        );
+    }
+
+    #[test]
+    fn non_peculiar_identifier_test() {
+        assert_eq!(non_peculiar("a"), Ok(("", "a")));
+        assert_eq!(non_peculiar("a+"), Ok(("", "a+")));
+        assert_eq!(non_peculiar("&a+"), Ok(("", "&a+")));
+        assert_eq!(
+            non_peculiar("+&a+"),
+            Err(NomErrorEnum(NomErrorStruct::new("+&a+", ErrorKind::OneOf)))
+        );
+    }
+
+    #[test]
+    fn parse_identifier_test() {
+        assert_eq!(
+            parse_identifier("...\n"),
+            Ok(("\n", Token::Identifier("...")))
+        );
+        assert_eq!(
+            parse_identifier("var\n"),
+            Ok(("\n", Token::Identifier("var")))
+        );
+        assert_eq!(
+            parse_identifier("var "),
+            Ok((" ", Token::Identifier("var")))
+        );
+        assert_eq!(
+            parse_identifier("var)"),
+            Ok((")", Token::Identifier("var")))
+        );
+        assert_eq!(
+            parse_identifier("var;"),
+            Ok((";", Token::Identifier("var")))
+        );
+        assert_eq!(
+            parse_identifier("var\""),
+            Ok(("\"", Token::Identifier("var")))
+        );
+        assert_eq!(
+            parse_identifier("he++o "),
+            Ok((" ", Token::Identifier("he++o")))
+        );
+        assert_eq!(
+            parse_identifier("hel.o "),
+            Ok((" ", Token::Identifier("hel.o")))
+        );
+        assert_eq!(
+            parse_identifier("..."),
+            Err(NomErrorEnum(NomErrorStruct::new("", ErrorKind::OneOf)))
+        );
+        assert_eq!(
+            parse_identifier("asdf,"),
+            Err(NomErrorEnum(NomErrorStruct::new(",", ErrorKind::OneOf)))
+        );
+    }
+
+    #[test]
+    fn parse_number_test() {
+        assert_eq!(
+            parse_number("+3.14;"),
+            Ok((";", Token::Number(LispNum::Float(3.14))))
+        );
+        assert_eq!(
+            parse_number("-3.14;"),
+            Ok((";", Token::Number(LispNum::Float(-3.14))))
+        );
+        assert_eq!(
+            parse_number("3.14;"),
+            Ok((";", Token::Number(LispNum::Float(3.14))))
+        );
+        assert_eq!(
+            parse_number(".14;"),
+            Ok((";", Token::Number(LispNum::Float(0.14))))
+        );
+        assert_eq!(
+            parse_number("1;"),
+            Ok((";", Token::Number(LispNum::Integer(1))))
+        );
+        assert_eq!(
+            parse_number("-1;"),
+            Ok((";", Token::Number(LispNum::Integer(-1))))
+        );
+        assert_eq!(
+            parse_number("-1;"),
+            Ok((";", Token::Number(LispNum::Integer(-1))))
+        );
+        assert_eq!(
+            parse_number("4294967296;"),
+            Err(NomErrorEnum(NomErrorStruct::new(";", ErrorKind::TooLarge)))
+        );
+    }
+
+    #[test]
+    fn parse_punctuator_test() {
+        assert_eq!(parse_punctuator(",3"), Ok(("3", Token::Punctuator(","))));
+        assert_eq!(parse_punctuator(",@"), Ok(("", Token::Punctuator(",@"))));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
-mod lexer;
+//! Library for lexing and parsing a reasonable subset of
+//! [R5RS](https://schemers.org/Documents/Standards/R5RS/r5rs.pdf) Scheme.
+//!
+//! ## Usage
+//! TO ADD
+
+pub mod lexer;
 mod parser;
-// mod code_generator;
