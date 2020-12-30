9faccb93f56c942a8a37775d9d96b3580dcad5eb
Sayantan Khan <sayantangkhan@gmail.com>
2020-12-30 04:02:50 -0500

    Started parser. Need to generalize TokenStream

---
+++ src/parser.rs
@@ -0,0 +1,23 @@
+//! Module to succesively parse a stream of `Token`s into `Expression`s and then `Program`.
+
+use crate::lexer::LispNum;
+use crate::reader::TokenStream;
+
+enum Datum {
+    Boolean(bool),
+    Number(LispNum),
+    Character(char),
+    String(String),
+    Identifier(String),
+    List(Vec<Datum>),
+    DottedPair(Vec<Datum>, Box<Datum>),
+    Quote(Box<Datum>),
+    BackQuote(Box<Datum>),
+    Comma(Box<Datum>),
+    CommaAt(Box<Datum>),
+    Vector(Vec<Datum>),
+}
+
+// struct DatumStream {
+//     token_stream: TokenStream,
+// }
